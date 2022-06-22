5d0c98cecfce3c1e68c00d25a74b983d5ed43796
Shady Alfred <shaadi.alfred@gmail.com>
2022-06-22 02:54:56 +0200

    Time: 196 ms (68.39%), Space: 41.9 MB (55.67%) - LeetHub

---
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
@@ -0,0 +1,33 @@
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        int answer = 0;
+        
+        int countOnes = 0;
+        int maxCountOnes = 0;
+        
+        int l = 0;
+        for(int r = 0; r < nums.Length; r++)
+        {
+            if (nums[r] == 1)
+            {
+                countOnes++;
+                maxCountOnes = Math.Max(countOnes, maxCountOnes);
+            }
+            
+            if (r - l + 1 - countOnes > k)
+            {
+                if (nums[l] == 1)
+                {
+                    countOnes--;
+                }
+                l++;
+            }
+            
+            answer = Math.Max(answer, r - l + 1);
+        }
+        return answer;
+        
+    }
+}
