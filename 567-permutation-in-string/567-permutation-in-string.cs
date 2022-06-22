3f2da4e8a72697d49871a4f997ca2d14887d703a
Shady Alfred <shaadi.alfred@gmail.com>
2022-06-22 03:25:58 +0200

    Time: 125 ms (51.76%), Space: 38.3 MB (61.35%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cs
+++ 567-permutation-in-string/567-permutation-in-string.cs
@@ -2,66 +2,50 @@ public class Solution
 {
     public bool CheckInclusion(string s1, string s2)
     {
-        var targetCount = new Dictionary<char, int>();
+        var freq = new Dictionary<char, int>();
+        
         foreach (char c in s1)
         {
-            targetCount[c] = targetCount.GetValueOrDefault(c, 0) + 1;
+            freq[c] = freq.GetValueOrDefault(c, 0) + 1;
         }
-        
+
         int l = 0;
-        var currentCount = new Dictionary<char, int>();
         
-        //"adc"
-        //"dcda" 
-        for(int r = 0; r < s2.Length; r++)
+        int matches = 0;
+
+        for (int r = 0; r < s2.Length; r++)
         {
-            currentCount[s2[r]] = currentCount.GetValueOrDefault(s2[r], 0) + 1;
-            
-            int len = r - l + 1;
-            
-            if (len > s1.Length || currentCount[s2[r]] > targetCount.GetValueOrDefault(s2[r], 0))
+            char rightChar = s2[r];
+            if (freq.ContainsKey(rightChar))
             {
-                currentCount[s2[l]]--;
-                if (currentCount[s2[l]] == 0)
+                freq[rightChar]--;
+                if (freq[rightChar] == 0)
                 {
-                    currentCount.Remove(s2[l]);
+                    matches++;
+                }
+            }
+            
+            if (r - l + 1 > s1.Length)
+            {
+                char leftChar = s2[l];
+                if (freq.ContainsKey(leftChar))
+                {
+                    if (freq[leftChar] == 0)
+                    {
+                        matches--;
+                    }
+                    freq[leftChar]++;
                 }
                 l++;
             }
             
-            
-            if (r - l + 1 == s1.Length)
+            if (matches == freq.Keys.Count)
             {
-                if (AreEqual(currentCount, targetCount))
-                {
-                    return true;
-                }
+                return true;
             }
+            
         }
-        
+
         return false;
-    }
-    
-    private bool AreEqual(Dictionary<char, int> dic1, Dictionary<char, int> dic2)
-    {
-        if (dic1.Keys.Count != dic2.Keys.Count)
-        {
-            return false;
-        }
-        
-        foreach (var key in dic1.Keys)
-        {
-            if (! dic2.ContainsKey(key))
-            {
-                return false;
-            }
-            
-            if (dic1[key] != dic2[key])
-            {
-                return false;
-            }
-        }
-        
-        return true;
     }
 }
