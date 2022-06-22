83a727575e5487933cf3ace8ea4e9173fafd7746
Shady Alfred <shaadi.alfred@gmail.com>
2022-06-23 00:38:22 +0200

    Time: 457 ms (95.67%), Space: 60.1 MB (14.98%) - LeetHub

---
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cs
@@ -0,0 +1,36 @@
+public class Solution
+{
+    public int[] MaxSlidingWindow(int[] nums, int k)
+    {
+        int[] maximumValues = new int[nums.Length - k + 1];
+        
+        var indeciesOfMaxValues = new LinkedList<int>();
+        
+        int l = 0;
+        for (int r = 0; r < nums.Length; r++)
+        {
+            while (indeciesOfMaxValues.Count > 0 &&
+                   nums[r] > nums[indeciesOfMaxValues.Last.Value])
+            {
+                indeciesOfMaxValues.RemoveLast();
+            }
+            
+            indeciesOfMaxValues.AddLast(r);
+            
+            if (r - l + 1 == k)
+            {
+                maximumValues[l] = nums[indeciesOfMaxValues.First.Value];
+                
+                if (indeciesOfMaxValues.Count > 0 &&
+                    l == indeciesOfMaxValues.First.Value)
+                {
+                    indeciesOfMaxValues.RemoveFirst();
+                }
+                    
+                l++;
+            }
+        }
+        
+        return maximumValues;
+    }
+}
