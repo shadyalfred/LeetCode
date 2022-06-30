98b27e029ce4686b938d46bb4c9ab4d72aec4d6a
Shady Alfred <shaadi.alfred@gmail.com>
2022-06-30 05:07:31 +0200

    Time: 164 ms (21.64%), Space: 40.9 MB (51.48%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.cs
@@ -0,0 +1,65 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode
+ * {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null)
+ *     {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public void ReorderList(ListNode head)
+    {
+        if (head == null || head.next == null)
+        {
+            return;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode firstHead = head;
+        ListNode secondHead = Reverse(slow);
+        
+        while (firstHead != null && secondHead != null)
+        {
+            ListNode temp = firstHead.next;
+            firstHead.next = secondHead;
+            firstHead = temp;
+            
+            temp = secondHead.next;
+            secondHead.next = firstHead;
+            secondHead = temp;
+        }
+        
+        if (firstHead != null && firstHead.next != null)
+        {
+            firstHead.next = null;
+        }
+    }
+    private ListNode Reverse(ListNode head)
+    {
+        ListNode prev = null;
+        
+        while (head != null)
+        {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+}
