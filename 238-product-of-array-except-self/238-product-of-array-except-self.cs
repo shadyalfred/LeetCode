03b792d23f94482baea507ca5d3b9819e35ff746
Shady Alfred <shaadi.alfred@gmail.com>
2022-07-04 19:26:39 +0200

    Time: 257 ms (35.75%), Space: 48.6 MB (94.41%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cs
@@ -0,0 +1,31 @@
+public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int N = nums.Length;
+        int[] prefix = new int[N];
+        int[] postfix = new int[N];
+        int[] answer = new int[N];
+        
+        for (int i = 0; i < N; i++)
+        {
+            if (i == 0)
+            {
+                prefix[i] = 1;
+                postfix[N - 1] = 1;
+                continue;
+            }
+            
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+            postfix[N - i - 1] = postfix[N - i] * nums[N - i];
+        }
+        
+        for (int i = 0; i < N; i++)
+        {
+            answer[i] = prefix[i] * postfix[i];
+        }
+        
+        return answer;
+        
+    }
+}
