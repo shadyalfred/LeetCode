7fc44b405a46045e90f2cd5dc515c7442cb1afdf
Shady Alfred <shadyalfred98@gmail.com>
2022-08-04 11:48:49 +0200

    Time: 8 ms (81.63%), Space: 2 MB (69.33%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.rs
+++ 9-palindrome-number/9-palindrome-number.rs
@@ -1,22 +1,19 @@
 impl Solution {
     pub fn is_palindrome(mut x: i32) -> bool {
-        if (x < 0) {
-            return false;
-        }
-        
-        let mut digits: Vec<i32> = Vec::with_capacity(10);
-        
-        while x != 0 {
-            digits.push(x % 10);
-            x /= 10;
-        }
-        
-        for i in 0..(digits.len() / 2) {
-            if (digits[i] != digits[digits.len() - 1 - i]) {
+        let x = x.to_string();
+        let x = x.as_bytes();
+
+        let mut l = 0;
+        let mut r = x.len() - 1;
+
+        while l < r {
+            if x[l] != x[r] {
                 return false;
             }
+            l += 1;
+            r -= 1;
         }
-        
+
         true
     }
 }
