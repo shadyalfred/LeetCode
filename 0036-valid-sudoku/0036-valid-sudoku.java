fd7346d3a16fba6db95cc4df05676a0f3b72e08f
Shady Alfred <shadyalfred98@gmail.com>
2022-10-29 19:55:36 +0200

    Time: 6 ms (68.41%), Space: 47.1 MB (50.38%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,71 +1,76 @@
 class Solution {
-
     public boolean isValidSudoku(char[][] board) {
         int rows = board.length;
         int cols = board[0].length;
-
-        Set<Character> rowSet = null;
-        Set<Character> colSet = null;
-
-        //check for rows
+        
+        // check rows
+        Set<Character> seen = null;
+        
         for (int i = 0; i < rows; i++) {
-            rowSet = new HashSet<>();
+            seen = new HashSet<>();
             for (int j = 0; j < cols; j++) {
                 if (board[i][j] == '.') {
                     continue;
                 }
-                if (rowSet.contains(board[i][j])) {
+                
+                if (seen.contains(board[i][j])) {
                     return false;
                 }
-                rowSet.add(board[i][j]);
+                
+                seen.add(board[i][j]);
             }
         }
-
-        //check for cols
+        
+        // check cols
         for (int i = 0; i < cols; i++) {
-            colSet = new HashSet<>();
+            seen = new HashSet<>();
             for (int j = 0; j < rows; j++) {
                 if (board[j][i] == '.') {
                     continue;
                 }
-                if (colSet.contains(board[j][i])) {
+                
+                if (seen.contains(board[j][i])) {
                     return false;
                 }
-
-                colSet.add(board[j][i]);
+                
+                seen.add(board[j][i]);
             }
         }
-
-        //block
-        for (int i = 0; i < rows; i = i + 3) {
-            for (int j = 0; j < cols; j = j + 3) {
+        
+        seen = null;
+        
+        // check blocks
+        for (int i = 0; i < rows; i += 3) {
+            for (int j = 0; j < cols; j += 3) {
                 if (!checkBlock(i, j, board)) {
                     return false;
                 }
             }
         }
-
+        
         return true;
     }
-
-    public boolean checkBlock(int idxI, int idxJ, char[][] board) {
-        Set<Character> blockSet = new HashSet<>();
-        int rows = idxI + 3;
-        int cols = idxJ + 3;
-        for (int i = idxI; i < rows; i++) {
-            for (int j = idxJ; j < cols; j++) {
+    
+    private boolean checkBlock(int row, int col, char[][] board) {
+        Set<Character> seen = new HashSet<>();
+        
+        int rows = row + 3;
+        int cols = col + 3;
+        
+        for (int i = row; i < rows; i++) {
+            for (int j = col; j < cols; j++) {
                 if (board[i][j] == '.') {
                     continue;
                 }
-
-                if (blockSet.contains(board[i][j])) {
+                
+                if (seen.contains(board[i][j])) {
                     return false;
                 }
-
-                blockSet.add(board[i][j]);
+                
+                seen.add(board[i][j]);
             }
         }
-
+        
         return true;
     }
 }
