4fd976d16d62c0c2982cf606d9dbac1a24c3d184
Shady Alfred <shadyalfred98@gmail.com>
2024-06-11 20:06:44 +0300

    Time: 6 ms (8.52%), Space: 42.3 MB (23.04%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,11 +1,18 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] indexOf = new int[1001] ;
+        Arrays.fill(indexOf, -1);
+        
+        for (int i = 0; i < arr2.length; i++) {
+            indexOf[arr2[i]] = i;
+        }
+
         return Arrays.stream(arr1).
             boxed().
             sorted(
                 (el1, el2) -> {
-                    Integer i1 = findElInArr(el1, arr2);
-                    Integer i2 = findElInArr(el2, arr2);
+                    Integer i1 = indexOf[el1];
+                    Integer i2 = indexOf[el2];
     
                     boolean el1ExistsInArr2 = i1 != -1;
                     boolean el2ExistsInArr2 = i2 != -1;
@@ -23,15 +30,5 @@ class Solution {
             ).
             mapToInt(i -> i).
             toArray();
-    }
-
-    private int findElInArr(int el, int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == el) {
-                return i;
-            }
-        }
-
-        return -1;
     }
 }
