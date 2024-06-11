f537d0a0ef38e42fd8d9554c53f7799fe6a91db8
Shady Alfred <shadyalfred98@gmail.com>
2024-06-11 22:19:03 +0300

    Time: 1 ms (73.84%), Space: 42.1 MB (45.23%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length];
+
         Integer[] elCount = new Integer[1001];
 
         for (int el : arr2) {
             elCount[el] = 0;
         }
 
-        List<Integer> endEls = new ArrayList<>(arr1.length);
+        int j = arr1.length - 1;
 
         for (int el : arr1) {
             if (elCount[el] != null) {
                 elCount[el]++;
             } else {
-                endEls.add(el);
+                result[j--]= el;
             }
         }
 
-        Collections.sort(endEls);
+        Arrays.sort(result, j + 1, result.length);
 
-        int j = 0;
+        j = 0;
 
         for (int el : arr2) {
             for (int i = 0; i < elCount[el]; i++) {
-                arr1[j++] = el;
+                result[j++] = el;
             }
         }
 
-        for (int el : endEls) {
-            arr1[j++] = el;
-        }
-
-        return arr1;
+        return result;
     }
 }
