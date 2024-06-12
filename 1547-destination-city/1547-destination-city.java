99e3a8f454b96879d4492767dc321088fec464de
Shady Alfred <shadyalfred98@gmail.com>
2024-06-12 21:23:52 +0300

    Time: 2 ms (99.83%), Space: 43.8 MB (10.35%) - LeetHub

--- 1547-destination-city/1547-destination-city.java
+++ 1547-destination-city/1547-destination-city.java
@@ -1,24 +1,18 @@
 class Solution {
     public String destCity(List<List<String>> paths) {
-        Map<String, Set<String>> adj = new HashMap<>();
-        Set<String> cities = new HashSet<>();
+        Set<String> leftSideCities = new HashSet<>();
+        Set<String> rightSideCities = new HashSet<>();
 
         for (List<String> path : paths) {
-            String citySrc = path.get(0);
-            String cityDst = path.get(1);
+            String leftCity = path.get(0);
+            String rightCity = path.get(1);
 
-            if (! adj.containsKey(citySrc)) {
-                adj.put(citySrc, new HashSet<>());
-            }
-
-            adj.get(citySrc).add(cityDst);
-
-            cities.add(citySrc);
-            cities.add(cityDst);
+            leftSideCities.add(leftCity);
+            rightSideCities.add(rightCity);
         }
 
-        for (String city : cities) {
-            if (!adj.containsKey(city)) {
+        for (String city : rightSideCities) {
+            if (!leftSideCities.contains(city)) {
                 return city;
             }
         }
