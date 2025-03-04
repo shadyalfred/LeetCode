697f09a747b603cd1b961361f2d9d7a520229edc
Shady Alfred <shadyalfred98@gmail.com>
2025-03-04 18:12:10 +0200

    Time: 5 ms (11.63%), Space: 40.9 MB (14.24%) - LeetHub

--- 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.java
@@ -1,9 +1,12 @@
 class Solution {
     private int n;
-    private int[] pwrs;
+    
+    private boolean dfs(int acc, int e) {
+        if (acc == n) {
+            return true;
+        }
 
-    private boolean dfs(int acc, int i) {
-        if (i >= pwrs.length) {
+        if (e > n) {
             return false;
         }
 
@@ -11,20 +14,13 @@ class Solution {
             return false;
         }
 
-        if (acc == n) {
-            return true;
-        }
+        int newE = e * 3;
 
-        return dfs(acc + pwrs[i], i + 1) || dfs(acc, i + 1);
+        return dfs(acc + e, newE) || dfs(acc, newE);
     }
 
     public boolean checkPowersOfThree(int n) {
         this.n = n;
-        this.pwrs = new int[17];
-        pwrs[0] = 1;
-        for (int i = 1; i < pwrs.length; i++) {
-            pwrs[i] = pwrs[i - 1] * 3;
-        }
-        return dfs(0, 0);
+        return dfs(0, 1);
     }
 }
