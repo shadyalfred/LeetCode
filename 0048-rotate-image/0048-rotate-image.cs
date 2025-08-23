633ae3e5376a0f6bf9cfb849d50571ffa98d8d85
Shady Alfred <shadyalfred98@gmail.com>
2025-08-23 20:52:14 +0300

    Time: 0 ms (100%), Space: 47.1 MB (21.24%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cs
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -1,18 +1,20 @@
 public class Solution {
     public void Rotate(int[][] matrix) {
         int n = matrix.Length;
-        int[][] m = new int[n][];
 
-        for (int i = 0; i < n; i++) {
-            m[i] = new int[n];
+        for (int i = 0; i < n/2; i++) {
             for (int j = 0; j < n; j++) {
-                m[i][j] = matrix[i][j];
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - i - 1][j];
+                matrix[n - i - 1][j] = temp;
             }
         }
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = m[n - j - 1][i];
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
