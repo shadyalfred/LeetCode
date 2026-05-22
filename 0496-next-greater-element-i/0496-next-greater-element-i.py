83de4e4f01035e8bcdf60787b1aef1c55c780136
Shady Alfred <shadyalfred98@gmail.com>
2026-05-22 18:43:54 +0300

    0496-NEXT-GREATER-ELEMENT-I Stats: Time: 0 ms (100%), Space: 19.4 MB (28.38%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,28 +1,12 @@
 class Solution:
   def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-    ans = []
+    stack = []
+    next_greater_element = dict()
 
-    index_of = dict()
-    for i, num in enumerate(nums2):
-      index_of[num] = i + 1
+    for num in nums2[::-1]:
+      while stack and stack[-1] <= num:
+        stack.pop()
+      next_greater_element[num] = stack[-1] if stack else -1
+      stack.append(num)
     
-    for num in nums1:
-      i = index_of.get(num) or -1
-      if i == -1:
-        ans.append(i)
-        continue
-      
-      was_found = False
-      while i < len(nums2):
-        if nums2[i] >= num:
-          was_found = True
-          ans.append(nums2[i])
-          break
-        i += 1
-        
-      if not was_found:
-        ans.append(-1)
-    
-    return ans
-
-
+    return [next_greater_element[num] for num in nums1]
